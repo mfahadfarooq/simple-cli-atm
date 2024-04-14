6f87a5939a89ca649fd14a4c3619ad5bfa8fe793
muhammadfahadfarooq <fahad.farooq7013@gmail.com>
2024-04-14 16:48:51 +0500

    init

---
+++ index.ts
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+
+
+let userBalance = 10000;
+let userPin = 1234;
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name:"pin",
+        message:"Enter you PIN",
+        type:"number"
+    }
+]);
+
+if(pinAnswer.pin == userPin)
+    {
+
+
+        let operationAns = await inquirer.prompt([
+            {
+                name:"operation",
+                message:"Please select option",
+                type:"list",
+                choices:["withdraw","check balance"]
+            }
+        ]);
+        if(operationAns.operation === "withdraw")
+        {
+            let withdrawAns = await inquirer.prompt([
+                {
+                    name:"amount",
+                    message:"Please enter amount",
+                    type:"number",
+                }
+            ]);
+            if(userBalance>= withdrawAns.amount)
+            {
+                userBalance = userBalance - withdrawAns.amount;
+                console.log(`Success , your balance is ${userBalance}`);
+
+            }
+            else{
+                console.log("Not Enough Balance!");
+            }
+
+        }
+        else  if(operationAns.operation === "check balance")
+        {
+            console.log(`Your balance is: ${userBalance}`);
+        }
+    
+    
+    }
+    else{
+        console.log("Invalid PIN!");
+    }
